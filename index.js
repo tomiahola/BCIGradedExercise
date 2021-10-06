bd33f9fdd01a611c5a26d20a63401ec137b91d94
tomiahola <--global>
2021-10-06 20:39:51 +0300

    5

--- index.js
+++ index.js
@@ -224,5 +224,5 @@ app.put('/postings/:id', passport.authenticate('jwt', {session: false}), (req, r
 })
 
 app.listen(app.get('port'), function() {
-    console.log(`Node app is running on port`, app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 })
